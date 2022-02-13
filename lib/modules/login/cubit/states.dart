90e48238498b98c4b76e8e924d454553405f8a71
Ahmed Esam <ahmedesam1369@gmail.com>
2022-02-14 00:58:26 +0200

    second commit

---
+++ lib/modules/login/cubit/states.dart
@@ -0,0 +1,18 @@
+abstract class LoginStates {}
+
+class LoginInitialState extends LoginStates {}
+class LoginLoadingState extends LoginStates {}
+
+class LoginSuccessState extends LoginStates {
+  late final String uId;
+  LoginSuccessState(
+    this.uId,
+  );
+
+}
+class LoginErrorState extends LoginStates {
+  late final String Error;
+  LoginErrorState(this.Error);
+}
+
+class ChangePasswordVisibilityState extends LoginStates {}
